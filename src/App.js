fd028de281122ebe77eb9a3ebe4ae8649675992e
JosueJMartinez <jmarti61@gmail.com>
2023-07-19 00:51:39 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@
 // import logo from './logo.svg';
 // import './App.css';
 import HeadContainer from './Components/HeadContainer';
+import SizeContainer from './Components/SizeContainer';
 
 // import './styles/custom-bootstrap.scss';
 
 function App() {
 	return (
 		<div className='App'>
-			<div className='container container-md d-flex align-items-center justify-content-center vh-100'>
+			<div className='desktop-background' />
+			<div className='container container-md d-flex flex-column align-items-center justify-content-center vh-100'>
 				<HeadContainer />
+				<SizeContainer />
 			</div>
 		</div>
 	);
